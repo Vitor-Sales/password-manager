003b128ceacc0e1fcec6dcec3d2fc07e8d1ec932
Vitor Sales <vitormalisa@gmail.com>
2023-11-30 17:44:26 -0300

    Task 03 done. button Cadastrar nova senha e onClick on button Cancelar e Cadastrar nova senha created

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,28 @@
+import { useState } from 'react';
+
 function Form() {
+  const [isShown, setIsShown] = useState(true);
+
   return (
-    <form>
-      <label htmlFor="nome-servico">Nome do serviço</label>
-      <input type="text" id="nome-servico" />
-      <label htmlFor="login">Login</label>
-      <input type="text" id="login" />
-      <label htmlFor="senha">Senha</label>
-      <input type="password" id="senha" />
-      <label htmlFor="url">URL</label>
-      <input type="text" id="url" />
-      <button>Cadastrar</button>
-      <button>Cancelar</button>
-    </form>
+    <>
+      {!isShown && (
+        <form>
+          <label htmlFor="nome-servico">Nome do serviço</label>
+          <input type="text" id="nome-servico" />
+          <label htmlFor="login">Login</label>
+          <input type="text" id="login" />
+          <label htmlFor="senha">Senha</label>
+          <input type="password" id="senha" />
+          <label htmlFor="url">URL</label>
+          <input type="text" id="url" />
+          <button>Cadastrar</button>
+          <button onClick={ () => setIsShown(true) }>Cancelar</button>
+        </form>)}
+      {isShown && (
+        <button onClick={ () => setIsShown(false) }>Cadastrar nova senha</button>
+      )}
+    </>
+
   );
 }
 
