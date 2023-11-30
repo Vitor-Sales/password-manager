e5cbda86477e0604081d5f07e97318e11a0bd819
Vitor Sales <vitormalisa@gmail.com>
2023-11-30 20:44:04 -0300

    Feat: Task 04 done. Button (Cadastrar) Validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,26 +2,68 @@ import { useState } from 'react';
 
 function Form() {
   const [isShown, setIsShown] = useState(true);
+  const [nameValue, setNameValue] = useState('');
+  const [loginValue, setLoginValue] = useState('');
+  const [URLValue, setURLValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+
+  function handleLoginChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setLoginValue(event.target.value.trim());
+  }
+  function handlePasswordChange(event: any) {
+    setPasswordValue(event.target.value.trim());
+  }
+  function handleURLChange(event: any) {
+    setURLValue(event.target.value.trim());
+  }
+  function validatePassword() {
+    const result = /(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).*/.test(passwordValue);
+    return passwordValue.length < 8 || passwordValue.length > 16 || !result;
+    //
+  }
 
   return (
-    <>
+    <div>
       {!isShown && (
         <form>
           <label htmlFor="nome-servico">Nome do serviço</label>
-          <input type="text" id="nome-servico" />
+          <input
+            type="text"
+            id="nome-servico"
+            onChange={ (e) => setNameValue(e.target.value) }
+            value={ nameValue }
+          />
           <label htmlFor="login">Login</label>
-          <input type="text" id="login" />
+          <input
+            type="text"
+            id="login"
+            onChange={ handleLoginChange }
+            value={ loginValue }
+          />
           <label htmlFor="senha">Senha</label>
-          <input type="password" id="senha" />
+          <input
+            type="password"
+            id="senha"
+            onChange={ handlePasswordChange }
+            value={ passwordValue }
+          />
           <label htmlFor="url">URL</label>
-          <input type="text" id="url" />
-          <button>Cadastrar</button>
-          <button onClick={ () => setIsShown(true) }>Cancelar</button>
+          <input type="text" id="url" onChange={ handleURLChange } value={ URLValue } />
+          <button
+            type="button"
+            disabled={ validatePassword()
+                || loginValue.length === 0
+                || nameValue.length === 0 }
+          >
+            Cadastrar
+
+          </button>
+          <button type="button" onClick={ () => setIsShown(true) }>Cancelar</button>
         </form>)}
       {isShown && (
         <button onClick={ () => setIsShown(false) }>Cadastrar nova senha</button>
       )}
-    </>
+    </div>
 
   );
 }
