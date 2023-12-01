80d557b7838bdc6ed429c17c78871d4ba627d324
Vitor Sales <vitormalisa@gmail.com>
2023-11-30 22:34:31 -0300

    Feat: Task 05 done. Validation display created.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -25,41 +25,85 @@ function Form() {
   return (
     <div>
       {!isShown && (
-        <form>
-          <label htmlFor="nome-servico">Nome do serviço</label>
-          <input
-            type="text"
-            id="nome-servico"
-            onChange={ (e) => setNameValue(e.target.value) }
-            value={ nameValue }
-          />
-          <label htmlFor="login">Login</label>
-          <input
-            type="text"
-            id="login"
-            onChange={ handleLoginChange }
-            value={ loginValue }
-          />
-          <label htmlFor="senha">Senha</label>
-          <input
-            type="password"
-            id="senha"
-            onChange={ handlePasswordChange }
-            value={ passwordValue }
-          />
-          <label htmlFor="url">URL</label>
-          <input type="text" id="url" onChange={ handleURLChange } value={ URLValue } />
-          <button
-            type="button"
-            disabled={ validatePassword()
-                || loginValue.length === 0
-                || nameValue.length === 0 }
-          >
-            Cadastrar
+        <div className="form-container">
+          <div>
+            <p>A senha deve:</p>
+            <ul className="password-requirements">
+              <li
+                className={ passwordValue.length < 8 ? 'invalid-password-check'
+                  : 'valid-password-check' }
+              >
+                Possuir 8 ou mais caracteres
+              </li>
+              <li
+                className={ passwordValue.length > 16 ? 'invalid-password-check'
+                  : 'valid-password-check' }
+              >
+                Possuir até 16 caracteres
 
-          </button>
-          <button type="button" onClick={ () => setIsShown(true) }>Cancelar</button>
-        </form>)}
+              </li>
+              <li
+                className={ !/(?=.*[a-zA-Z])(?=.*\d)/.test(passwordValue) ? 'invalid-password-check'
+                  : 'valid-password-check' }
+              >
+                Possuir letras e números
+
+              </li>
+              <li
+                className={ !/(?=.*[\W_])/.test(passwordValue) ? 'invalid-password-check'
+                  : 'valid-password-check' }
+              >
+                Possuir algum caractere especial
+
+              </li>
+            </ul>
+          </div>
+          <form className="form">
+            <label htmlFor="nome-servico">Nome do serviço</label>
+            <input
+              type="text"
+              id="nome-servico"
+              onChange={ (e) => setNameValue(e.target.value) }
+              value={ nameValue }
+            />
+            <label htmlFor="login">Login</label>
+            <input
+              type="text"
+              id="login"
+              onChange={ handleLoginChange }
+              value={ loginValue }
+            />
+            <label htmlFor="senha">Senha</label>
+            <input
+              type="password"
+              id="senha"
+              onChange={ handlePasswordChange }
+              value={ passwordValue }
+            />
+            <label htmlFor="url">URL</label>
+            <input type="text" id="url" onChange={ handleURLChange } value={ URLValue } />
+            <div>
+              <button
+                className="btn-register"
+                type="button"
+                disabled={ validatePassword()
+                    || loginValue.length === 0
+                    || nameValue.length === 0 }
+              >
+                Cadastrar
+              </button>
+              <button
+                className="btn-cancel"
+                type="button"
+                onClick={ () => setIsShown(true) }
+              >
+                Cancelar
+
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
       {isShown && (
         <button onClick={ () => setIsShown(false) }>Cadastrar nova senha</button>
       )}
